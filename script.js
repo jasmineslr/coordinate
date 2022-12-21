c874047e0e85c880619bc3a65f2f670e7a283c75
Jasmineslr <yasaman.akhtari@yahoo.com>
2022-12-21 13:30:28 +0330

    first version

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById("coordinate").onmousemove=function(e){
+    let x=e.clientX;
+    let y=e.clientY;
+    let result="X:" + x + "px" +'<br>'+ "Y:" + y + "px";
+    document.q
+ document.querySelector(".xy").innerHTML=result
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
